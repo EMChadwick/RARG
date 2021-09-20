63ac89380a0414787fe614219a06c63f5bb9722b
Ed Chadwick <edwardmchadwick@gmail.com>
2021-09-20 17:51:19 +0100

    Update RARG.js

--- RARG.js
+++ RARG.js
@@ -2,5 +2,7 @@ $( document ).ready(function() {
 	$("#startButton").click(function() {
 		alert( "Oops, there's nothing here" );
 	});
-	var splashJson = $.getJSON("splash.JSON");
+	var splashJson = $.getJSON("splash.JSON", function(splashData){
+		$("#splashBanner marquee").html(splashData[0]);
+	});
 });
